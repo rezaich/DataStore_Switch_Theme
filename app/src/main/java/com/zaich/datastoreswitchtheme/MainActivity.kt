ab0622c34bd02b8af8346ce9f118b6834c246d38
rezaich <ichsanireza@gmail.com>
2021-09-15 20:40:39 +0700

    Initial commit

---
+++ app/src/main/java/com/zaich/datastoreswitchtheme/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.zaich.datastoreswitchtheme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
+
+        switchTheme.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+        }
+
+        }
+    }
+
+
+}
